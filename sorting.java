05af4389d7902031088fb29340cc9d9b5439b968
Srujana-Elukurthi <srujanav2204@gmail.com>
2025-08-11 21:41:13 +0530

    Create sorting.java

---
+++ sorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SortExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 3};
+
+        // Built-in sort
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted array (built-in): " + Arrays.toString(numbers));
+    }
+}
