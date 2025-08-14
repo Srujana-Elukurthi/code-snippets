90f6e64f9f4f97f97348207b9d502e7a5e49d217
Srujana-Elukurthi <srujanav2204@gmail.com>
2025-08-14 22:23:22 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,8 @@
+// Select by ID
+let title = document.getElementById("title");
+
+// Select by Class
+let message = document.querySelector(".message");
+
+// Select by Tag
+let paragraphs = document.getElementsByTagName("p");
